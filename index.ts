746fcb0ad761963d29d67940f25b4d024e8a3735
pelin eliacik <pelineliacik88@gmail.com>
2024-05-06 13:56:23 +0300

    test

---
+++ index.ts
@@ -0,0 +1,48 @@
+
+//x=5;
+//console.log(x);
+
+//x=true;
+///console.log(x);
+
+//x=10.5;
+//console.log(x);
+
+//x=[1,2,3,4];
+//console.log(x);
+// function varKeyword()
+// {
+//     var z=1;
+//     if(true)
+//         {
+//            var z=5;
+//            console.log(z);
+//         }
+//         console.log(z);
+// }
+// function letKeyword()
+// {
+//     let z=1;
+//     if(true)
+//         {
+//            let z=5;
+//            console.log(z);
+//         }
+//         console.log(z);
+// }
+// varKeyword();
+// console.log("*******");
+// letKeyword();
+
+console.log("-----");
+
+
+// var myVariable=50;
+// let myVariable2=100;
+// console.log(myVariable2);
+
+// console.log(myVariable);
+
+console.log("Abcde");
+
+console.log("sdfg");
